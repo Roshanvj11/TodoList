1a70c37193e44266acae077089c1f9c9e4568a73
Roshanvj11 <sroshanvijay@gmail.com>
2024-10-06 19:52:45 +0530

    main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import Main from "./registor/Main";
+import Login from "./registor/Login";
+import Registor from "./registor/Registor";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>todo list</h1>
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/TodoList" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registor" element={<Registor />} />
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
