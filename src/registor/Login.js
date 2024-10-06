869a50fb262e12d303d331cd1c5b9677d8524ef7
Roshanvj11 <sroshanvijay@gmail.com>
2024-10-06 20:29:03 +0530

    page Modification

--- src/registor/Login.js
+++ src/registor/Login.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function Login() {
+  const navigate = useNavigate();
+  const handleClick=(navigationPage)=>{
+    navigate(navigationPage)
+  }
   return (
-    <div>Login</div>
+    <div className='Login'>
+
+      <div className='LoginForm'>
+
+        <div>
+          <h1>Login</h1>
+        </div>
+
+        <div>
+          <TextField id="standard-basic" label="Email" variant="standard" />
+          <TextField id="standard-basic" label="Password" variant="standard" />
+          <p>Forgot Password ?</p>
+        </div>
+
+        <div>
+          <Button variant="contained">Login</Button>
+        </div>
+
+        <div>
+          <p>Not a Member ?<Button onClick={()=>handleClick('/registor')} href="#text-buttons">Signup</Button></p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
--- src/registor/Registor.js
+++ src/registor/Registor.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function Registor() {
+  const navigate = useNavigate();
+  const handleClick = (navigationPage) => {
+    navigate(navigationPage)
+  }
   return (
-    <div>Registor</div>
+    <div className='Registor'>
+
+      <div className='RegistorForm'>
+
+        <div>
+          <h1>Sign Up</h1>
+        </div>
+
+        <div>
+          <TextField id="standard-basic" label="Email" variant="standard" />
+          <TextField id="standard-basic" label="Password" variant="standard" />
+          <TextField id="standard-basic" label="Confirm Password" variant="standard" />
+        </div>
+
+        <div>
+          <Button variant="contained" onClick={() => handleClick('/login')}>Sign Up</Button>
+        </div>
+
+        <div>
+          <p>Already Have an Account ? <Button onClick={() => handleClick('/login')} href="#text-buttons">Login</Button></p>
+        </div>
+
+
+      </div>
+
+    </div>
   )
 }
