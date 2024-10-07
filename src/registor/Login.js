3808e705c62fd54992ff4e03d04747d5c2d51b81
Roshanvj11 <sroshanvijay@gmail.com>
2024-10-07 13:08:49 +0530

    form validation

--- src/registor/Login.js
+++ src/registor/Login.js
@@ -2,14 +2,22 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 
 
 export default function Login() {
   const navigate = useNavigate();
-  const handleClick=(navigationPage)=>{
+  const handleClick = (navigationPage) => {
     navigate(navigationPage)
   }
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('data', data)
+  }
+
   return (
     <div className='Login'>
 
@@ -19,18 +27,40 @@ export default function Login() {
           <h1>Login</h1>
         </div>
 
-        <div>
-          <TextField id="standard-basic" label="Email" variant="standard" />
-          <TextField id="standard-basic" label="Password" variant="standard" />
-          <p>Forgot Password ?</p>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+
+            <div>
+              <TextField id="standard-basic" label="Email" variant="standard"
+                {...register('email', {
+                  required: 'email is required',
+                  pattern: {
+                    value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                    message: 'email is not valid'
+                  }
+                })} />
+              {errors.email && <p className='errorMsg' style={{ color: 'red' }}>{errors.email.message}</p>}
+            </div>
+
+            <div>
+              <TextField id="standard-basic" label="Password" variant="standard"
+                {...register('password', {
+                  required: 'password is required'
+                })} />
+              {errors.password && <p className='errorMsg' style={{ color: 'red' }}> {errors.password.message} </p>}
+            </div>
+
+
+          </div>
+
+          <div>
+            <Button variant="contained" type='submit'>Login</Button>
+          </div>
+
+        </form>
 
         <div>
-          <Button variant="contained">Login</Button>
-        </div>
-
-        <div>
-          <p>Not a Member ?<Button onClick={()=>handleClick('/registor')} href="#text-buttons">Signup</Button></p>
+          <p>Not a Member ?<Button onClick={() => handleClick('/registor')} href="#text-buttons">Signup</Button></p>
         </div>
 
       </div>
